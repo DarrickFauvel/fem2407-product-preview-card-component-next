6c439522c0241e1068b32adafaf4b11ce11665d5
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-25 05:23:10 -0400

    Remove rounding from image container

--- app/components/card/components/image/index.tsx
+++ app/components/card/components/image/index.tsx
@@ -4,7 +4,7 @@ import imageMobile from "@/assets/images/image-product-mobile.jpg"
 
 const CardImage = () => {
   return (
-    <div className="relative overflow-hidden rounded-t-xl">
+    <div className="relative overflow-hidden">
       <Image
         className="w-full h-full object-cover transition-all duration-500 group-hover:scale-110 group-hover:rotate-1"
         src={imageMobile}
