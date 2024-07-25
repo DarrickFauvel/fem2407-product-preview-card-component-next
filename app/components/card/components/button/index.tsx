b8cf5e99a24573a1cdfe40ab97e7ad37a6115bb5
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-25 17:46:55 -0400

    Adjust button icon width

--- app/components/card/components/button/index.tsx
+++ app/components/card/components/button/index.tsx
@@ -5,7 +5,7 @@ import imageCart from "@/assets/images/icon-cart.svg"
 const CardButton = () => {
   return (
     <button className="flex justify-center items-center gap-2 text-white text-sm font-bold py-3 sm:py-4 mt-2 rounded-lg transition-all duration-300 bg-fem-deep-aquamarine hover:bg-fem-deep-aquamarine-hover">
-      <Image src={imageCart} width={14.39} height={16} alt="cart icon" />
+      <Image src={imageCart} width={14} height={16} alt="cart icon" />
       <span>Add to Cart</span>
     </button>
   )
