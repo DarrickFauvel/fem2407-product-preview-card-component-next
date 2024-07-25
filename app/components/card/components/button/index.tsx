23b12d674ac3eac51b291fa1beff36e05b714841
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-25 17:41:43 -0400

    Adjust responsive button padding

--- app/components/card/components/button/index.tsx
+++ app/components/card/components/button/index.tsx
@@ -4,7 +4,7 @@ import imageCart from "@/assets/images/icon-cart.svg"
 
 const CardButton = () => {
   return (
-    <button className="flex justify-center items-center gap-2 text-white text-sm font-bold py-3 mt-2 rounded-lg transition-all duration-300 bg-fem-deep-aquamarine hover:bg-fem-deep-aquamarine-hover">
+    <button className="flex justify-center items-center gap-2 text-white text-sm font-bold py-3 sm:py-4 mt-2 rounded-lg transition-all duration-300 bg-fem-deep-aquamarine hover:bg-fem-deep-aquamarine-hover">
       <Image src={imageCart} width={14.39} height={16} alt="cart icon" />
       <span>Add to Cart</span>
     </button>
