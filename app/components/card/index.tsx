5b92d59426ad435773a1e092b21ef086140b9bd9
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-25 17:25:27 -0400

    Verticle justify center the card content on larger view

--- app/components/card/index.tsx
+++ app/components/card/index.tsx
@@ -12,7 +12,7 @@ const Card = () => {
   return (
     <article className="group flex flex-col sm:flex-row sm:w-[600px] my-7 mx-4 bg-white rounded-xl overflow-hidden">
       <CardImage />
-      <div className="flex flex-col gap-3 sm:w-1/2 p-6">
+      <div className="flex flex-col sm:justify-center gap-3 sm:w-1/2 p-6">
         <CardCategoryTitle>Perfume</CardCategoryTitle>
 
         <CardTitle>Gabrielle Essence Eau De Parfum</CardTitle>
