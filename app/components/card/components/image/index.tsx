285531b5045e8aa25f0a4f5574a4b1859b785752
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-25 06:01:58 -0400

    Add desktop image

--- app/components/card/components/image/index.tsx
+++ app/components/card/components/image/index.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image"
 
 import imageMobile from "@/assets/images/image-product-mobile.jpg"
+import imageDesktop from "@/assets/images/image-product-desktop.jpg"
 
 const CardImage = () => {
   return (
-    <div className="relative overflow-hidden">
+    <div className="relative overflow-hidden sm:w-1/2">
+      {/* Show this image on screens < 640px */}
       <Image
-        className="w-full h-full object-cover transition-all duration-500 group-hover:scale-110 group-hover:rotate-1"
+        className="w-full h-full object-cover transition-all duration-500 group-hover:scale-110 group-hover:rotate-1 sm:hidden"
         src={imageMobile}
         width={343}
         height={240}
+        alt="product photo"
+      />
+
+      {/* or */}
+      {/* Show this image on screens >= 640px */}
+      <Image
+        className="w-full h-full object-cover transition-all duration-500 group-hover:scale-110 group-hover:rotate-1 hidden sm:block"
+        src={imageDesktop}
+        width={300}
+        height={450}
         alt="product photo"
       />
     </div>
